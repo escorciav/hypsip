f16609fb5f57c7aa40a58a1fb1eb49275e991fbe
Victor Escorcia <escorciav@gmail.com>
2016-12-05 23:42:04 +0300

    [examples] Add simple programs

---
+++ examples/simple.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+
+
+def input_parser():
+    p = ArgumentParser(description='Evaluate Six Hump Camel Back')
+    p.add_argument('-x', '--x-var', required=True, type=float)
+    p.add_argument('-y', '--y-var', required=True, type=float)
+    return p
+
+
+def main(x_var, y_var, **kwargs):
+    """Six Hump Camel Back
+    """
+    f = ((4 - 2.1 * x_var**2 + x_var**4 / 3) * x_var**2 +
+         x_var * y_var + (-4 + 4 * y_var**2) * y_var**2)
+    return f
+
+
+if __name__ == '__main__':
+    p = input_parser()
+    print 'f:', main(**vars(p.parse_args()))
---
+++ examples/simple_similar.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+
+
+def input_parser_rosenbrock():
+    p = ArgumentParser(description='Evaluate Rosenbrock function')
+    p.add_argument('-x', '--x-var', required=True, type=float)
+    p.add_argument('-y', '--y-var', required=True, type=float)
+    return p
+
+
+def main_rosenbrock(x_var, y_var, a=1, b=100, **kwargs):
+    """Rosenbrock fuction
+    """
+    f = (a - x_var)**2 + b * (y_var - x_var**2)**2
+    return f
+
+
+if __name__ == '__main__':
+    p = input_parser_rosenbrock()
+    print 'f:', main_rosenbrock(**vars(p.parse_args()))
